23fb3c3a602710f1187d5be1a55528e3d4c71fe5
Bashshesh <115872700+Bashshesh@users.noreply.github.com>
2023-04-25 09:42:10 +0600

    Add files via upload

---
+++ src/MyList.java
@@ -0,0 +1,15 @@
+public interface MyList<T> {
+    int size();
+    boolean contains(Object o);
+    void add(T item);
+    void add(T item, int index);
+    boolean remove(T item);
+    T remove(int index);
+    void clear();
+    T get(int index);
+
+    void DelDub();
+    int indexOf(Object o) throws Exception;
+    int lastIndexOf(Object o);
+    void sort();
+}
