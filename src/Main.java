f11f6ee6ed1362e9c2343b809fba46beb5175a90
Bashshesh <115872700+Bashshesh@users.noreply.github.com>
2023-04-25 09:40:41 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(4);
-        arrayList.add(5);
-
-        System.out.println(arrayList.getSize());
-
-        arrayList.add(6);
-        System.out.println(arrayList.getSize());
-        System.out.println(arrayList.get(2));
-        System.out.println(arrayList.get(5));
-
-        arrayList.remove(2);
-        arrayList.remove(3);
-
-        LinkedList<String> list = new LinkedList<>();
-
-        list.add("a");
-        list.add("b");
-        list.add("c");
-
-        for (String element : list) {
-            System.out.print(element + " ");
-        }
-
+        MyArrayList<Integer> array = new MyArrayList<Integer>();
+        MyArrayList array1 = new MyArrayList();
+        array.add(0);
+        array.add(13);
+        array.add(11);
+        array.add(10);
+        array.add(12);
+        array.add(13);
+        array.add(10);
+        array.add(11);
+        array.getArray();
         System.out.println();
-        String second = list.get(1);
-        System.out.println(second);
-
-        String removed = list.remove(0);
-        System.out.println(removed);
-
-        for (String element : list) {
-            System.out.print(element + " ");
-        }
-        System.out.println(second);
-        System.out.println(list.getSize());
+        array.DeleteDub();
+        array.getArray();
     }
-
-
 }
